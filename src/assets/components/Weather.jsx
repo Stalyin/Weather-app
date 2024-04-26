8efacc5857a62a9fc5eb6fc3f790923a4837f06d
Stalyin <stalyin16ac@gmail.com>
2024-04-26 13:35:54 -0500

    Weather-app8

--- src/assets/components/Weather.jsx
+++ src/assets/components/Weather.jsx
@@ -19,23 +19,23 @@ const Weather = ({ weatherData }) => {
     switch (weatherDescription) {
       case "clear sky":
       case "hottest":
-        return "/calor.gif";
+        return "../image/calor.gif";
       case "light rain":
-        return "/lluvia.gif";
+        return "../image/lluvia.gif";
       case "few clouds":
       case "scattered clouds":
       case "broken clouds":
       case "overcast clouds":
-        return "/nublado.gif";
+        return "../image/nublado.gif";
       case "shower rain":
       case "rain":
-        return "/lluvia.gif";
+        return "../image/lluvia.gif";
       case "thunderstorm":
-        return "/truenos.gif";
+        return "../image/truenos.gif";
       case "coldest":
-        return "/a873dcd8c2eae1b8197cfbd4dc5f131c_w200.gif";
+        return "../image/a873dcd8c2eae1b8197cfbd4dc5f131c_w200.gif";
       default:
-        return "/despejado.gif";
+        return "../image/despejado.gif";
     }
   };
 
